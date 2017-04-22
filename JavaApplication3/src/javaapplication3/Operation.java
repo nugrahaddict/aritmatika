fe8f2957e4aa625cd13b4aa49638d87c3741011e
Adi Nugraha <Adi Nugraha>
2017-04-22 11:29:20 +0700

    saya menambahkan abstract class interface dan class

---
+++ JavaApplication3/src/javaapplication3/Operation.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Adi Nugraha
+ */
+public interface Operation {
+    public float operate(int x, int y);
+    public void display();
+}
